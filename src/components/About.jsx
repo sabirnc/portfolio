e730148343ca44a904fbc753c45589985fe1c1cd
sabircn <mohammedsabir5123@gmail.com>
2024-06-28 15:29:34 +0530

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,9 +47,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        I'm a skilled software developer with  3 experience in TypeScript ,
+        JavaScript,python, .Net and expertise in frameworks like React, Node.js, 
+        Next.js,.Net mvc , pandas , drf, Angular.js and Three.js. 
+        I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
